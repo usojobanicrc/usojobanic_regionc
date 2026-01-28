91473450483bfe4db1ab08630018cf9b6c45d609
usojobanicRC <usojobanicrc@gmail.com>
2026-01-27 21:01:39 -0600

    four commit: about us

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,56 +1,48 @@
-import React from 'react'
-import { Link } from "react-router-dom";
-import logo from '../img/usojobanicB.png'
+
+import React from 'react';
+import logo from '../img/usojobanicB.png';
 import "../css/Footer.css";
 
 export const Footer = () => {
-
-
   return (
-    <footer>
-      <p class='text-center'>Union de Asociasiones Bautistas de Nicaragua
-        Region Central</p>
+    <footer className="main-footer">
       <div className="container__footer">
+        {/* Sección del Logo y Descripción */}
         <div className="box__footer">
           <div className="logo">
             <img src={logo} alt="Usojobanic RC" />
           </div>
+          <div className="terms">
+            <p>
+              La Unión de Sociedades de Jóvenes Bautistas de Nicaragua (USOJOBANIC), 
+              es un Ministerio de la Convención Bautista de Nicaragua (CBN).
+            </p>
+          </div>
         </div>
 
+        {/* Sección Enlaces */}
         <div className="box__footer">
-
-          <h2>Sobre nosotros</h2>
+          <h2>Conócenos</h2>
           <a href="#!">Quienes Somos</a>
-          <a href="#!">Nuestro Proposito</a>
-          <Link to='/principios'>Nuestros principios</Link>
+          <a href="#!">Nuestro Misión</a>
+          <a href="#!">Nuestros principios</a>
+          <a href="#!">Junta Directiva</a>
         </div>
 
-        <div className="box__footer">
-
-          <h2>Conoce mas</h2>
-          <Link to='/juntadirectiva'>Junta Directiva</Link>
-          <Link to='/circuitos'>Circuitos</Link>
-          <a href="#!">Ultimos Eventos</a>
-        </div>
-
+        {/* Sección Redes Sociales */}
         <div className="box__footer">
           <h2>Redes Sociales</h2>
-          <div className='container-social-icons'>
-            <a href="https://www.facebook.com/share/17f5U1CMvC/"><i class="bi bi-facebook social-icon"></i></a>
-            <a href="https://www.tiktok.com/@usojobanic.region?is_from_webapp=1&sender_device=pc"><i class="bi bi-tiktok social-icon"></i></a>
-            <a href="#!"><i class="bi bi-google social-icon"></i></a>
-            <a href="https://www.instagram.com/usojobanic_rc?igsh=MWNmcGVoaGNwbnhtZg=="><i class="bi bi-instagram social-icon"></i></a>
-          </div>
-
+          <a href="https://www.facebook.com/share/17f5U1CMvC/"><i className="bi bi-facebook"></i> Facebook</a>
+          <a href="https://www.tiktok.com/@usojobanic.region?is_from_webapp=1&sender_device=pc"><i className="bi bi-tiktok"></i> Messenger</a>
+          <a href="emailto:usojobanicrc@gmail.com"><i className="bi bi-envelope-fill"></i> Gmail</a>
+          <a href="https://www.instagram.com/usojobanic_rc?igsh=MWNmcGVoaGNwbnhtZg=="><i className="bi bi-instagram"></i> Instagram</a>
         </div>
-
       </div>
 
       <div className="box__copyright">
-        <div className="styled-hr">
-          <hr /><p>Todos los derechos reservados © 2025 <b>USOJOBANIC Región Central</b></p>
-        </div>
+        <hr />
+        <p>Todos los derechos reservados © 2026 <b>USOJOBANIC Región Central</b></p>
       </div>
     </footer>
-  )
-}
+  );
+};
